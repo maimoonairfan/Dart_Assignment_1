9c32e2021966df255759ebbbfa6a6d9763417793
Maimoona M.Irfan <87384658+maimoonairfan@users.noreply.github.com>
2022-09-30 00:54:30 +0500

    Add files via upload

---
+++ Dart_Assignment1.dart
@@ -0,0 +1,34 @@
+void main() {
+//  Answer to Question1
+//   int length = 5;
+//   int breadth = 7;
+//   var formula = length*breadth;
+//   print("The area of rectangle = $formula"  );
+  
+//  Answer to Question2
+//   var num = 7;
+//   var i = num+8/3%5*5;
+//   print("The final result = $i");
+  
+//   Answer to Question3
+//   int a = 10;
+//   int b = 6;
+//   var c = a<50 && a < b;
+//   print("$c");
+//   var d = a<50 || a < b;
+//   print("$d");
+  
+//  Answer to Question4
+//   var name = 'Robert';  
+//   int maths = 78;
+//   int eng = 45;
+//   int urdu = 62;
+//   int total = maths + eng + urdu;
+//   double percentage = total/300*100;
+//   print("Marks of Maths = $maths");
+//   print("Marks of Eng = $eng");
+//   print("Marks ofUrdu = $urdu");
+//   print("Total Marks = $total");
+//   print("The percentage of $name is $percentage");
+  
+}
